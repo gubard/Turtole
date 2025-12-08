28094e46d6604c41024e08bd4728387b47498b7f
gubard <maksimovsergey08@gmail.com>
2025-12-08 15:32:23 +0200

    Simplify application startup by replacing custom configuration with `RunZeusApp`.

--- Turtle/Program.cs
+++ Turtle/Program.cs
@@ -1,68 +1,7 @@
-using Gaia.Helpers;
-using Gaia.Services;
-using Microsoft.EntityFrameworkCore;
-using Nestor.Db.Sqlite;
 using Turtle.Contract.Models;
 using Turtle.Contract.Services;
 using Zeus.Helpers;
-using Zeus.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddAuthorization();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddJwtAuthentication(builder.Configuration);
-builder.Services.AddTransient<IStorageService, StorageService>();
-builder.Services.AddTransient<ICredentialService, EfCredentialService>();
-builder.Services.AddTransient<IDbMigrator, DbMigrator>(sp => new(sp.GetRequiredService<IStorageService>().GetDbDirectory().Combine("Turtle")));
-
-builder.Services.AddDbContext<DbContext, SqliteNestorDbContext>((sp, options) =>
-{
-    var httpContextAccessor = sp.GetRequiredService<IHttpContextAccessor>();
-    var userId = httpContextAccessor.HttpContext.ThrowIfNull().GetUserId();
-    var dataSourceFile = sp.GetRequiredService<IStorageService>().GetDbDirectory().Combine("Turtle").ToFile($"{userId}.db");
-    options.UseSqlite($"Data Source={dataSourceFile}", x => x.MigrationsAssembly(typeof(SqliteNestorDbContext).Assembly));
-
-    if (dataSourceFile.Exists)
-    {
-        return;
-    }
-
-    if (dataSourceFile.Directory?.Exists != true)
-    {
-        dataSourceFile.Directory?.Create();
-    }
-
-    using var context = new SqliteNestorDbContext(options.Options);
-    context.Database.Migrate();
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapPost(RouteHelper.Get,
-        (TurtleGetRequest request, ICredentialService authenticationService, CancellationToken ct) =>
-            authenticationService.GetAsync(request, ct))
-   .RequireAuthorization()
-   .WithName(RouteHelper.GetName);
-
-app.MapPost(RouteHelper.Post,
-        (TurtlePostRequest request, ICredentialService authenticationService, CancellationToken ct) =>
-            authenticationService.PostAsync(request, ct))
-   .RequireAuthorization()
-   .WithName(RouteHelper.PostName);
-
-await app.Services.GetRequiredService<IDbMigrator>().MigrateAsync(CancellationToken.None);
-await app.RunAsync();
+await WebApplication.CreateBuilder(args)
+   .RunZeusApp<ICredentialService, EfCredentialService, TurtleGetRequest,
+        TurtlePostRequest, TurtleGetResponse, TurtlePostResponse>("Turtle");
